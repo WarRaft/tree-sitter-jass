7200488c4ed8162228385d2572ec08e7671ca6ac
nazarpunk <nazarpunk@gmail.com>
2025-11-16 22:38:43 +0200

    grammar 2

--- generate.sh
+++ generate.sh
@@ -1,10 +1,6 @@
+#!/bin/bash
+set -e
+
 tree-sitter generate
-
-#tree-sitter playground
-
-# https://tree-sitter.github.io/tree-sitter/creating-parsers/5-writing-tests.html
-#tree-sitter test
-#tree-sitter parse vjass/test.vjass
-
 tree-sitter build --wasm
 tree-sitter playground
--- grammar.js
+++ grammar.js
@@ -1,66 +1,34 @@
 const PREC = {
-    COMMA: -1,
-    FUNCTION: 1,
-    DEFAULT: 1,
-    PRIORITY: 2,
-
-    BIT_OR: 6, // => |
-    BIT_NOT: 7, // => ~
-    BIT_AND: 8, // => &
-    SHIFT: 9, // => << >>
-    CONCAT: 10, // => ..
-    PLUS: 11, // => + -
-    MULTI: 12, // => * /             // %
-    UNARY: 13, // => not # - ~
-    POWER: 14, // => ^
-
-    STATEMENT: 15,
-    PROGRAM: 16,
+    ASSIGNMENT: 1,      // = (lowest)
+    LOGICAL_AND: 12,    // and (lower in JASS)
+    LOGICAL_OR: 13,     // or (higher in JASS)
+    EQUALITY: 14,       // == !=
+    RELATIONAL: 15,     // < > <= >=
+    ADDITIVE: 16,       // + -
+    MULTIPLICATIVE: 17, // * /
+    UNARY: 18,          // not - + ++ --
+    POSTFIX: 19,        // ++ --
+    SUBSCRIPT: 20,      // []
 }
 
 module.exports = grammar({
     name: 'jass',
 
-    externals: $ => [
-        $._block_comment_start,
-        $._block_comment_content,
-        $._block_comment_end,
+    word: $ => $.id,
 
-        $._string_start,
-        $._string_content,
-        $._string_end,
-    ],
     extras: $ => [/\n/, /\s/, $.comment],
 
-    inline: $ => [
-        //$.expr,
-        //$.field_separator,
-        //$.prefix_exp,
-        //$.function_impl,
-        $.comment,
-    ],
 
     conflicts: $ => [
-        [$.id, $.keywords],
-        [$.expr, $.function_call]
+        [$.expr, $.function_call],
+        [$.loop_statement],
+        [$.if_statement],
     ],
 
     rules: {
         program: $ => repeat($._block),
 
-        id: _ => token(prec(-1, /[a-zA-Z_][a-zA-Z0-9_]*/)),
-        keywords: _ => choice(
-            'globals', 'endglobals',
-            'function', 'endfunction',
-            'native',
-            'type', 'extends',
-            'if', 'then', 'else', 'elseif', 'endif',
-            'loop', 'endloop',
-            'set', 'call', 'return', 'exitwhen',
-            'local', 'constant', 'array',
-            'takes', 'returns', 'nothing',
-            'and', 'or', 'not'
-        ),
+        id: _ => /[a-zA-Z_][a-zA-Z0-9_]*/,
 
         _block: $ => choice(
             $.globals,
@@ -69,107 +37,89 @@ module.exports = grammar({
             $.type_statement,
         ),
 
+        _statement: $ => choice(
+            $.if_statement,
+            $.loop_statement,
+            $.return_statement,
+            $.exitwhen_statement,
+            $.local_statement,
+            $.expr
+        ),
+
+        loop_statement: $ =>
+            seq(
+                'loop',
+                repeat($._statement),
+                optional('endloop')
+            ),
+
         globals: $ => seq(
             'globals',
             repeat($.var_stmt),
-            'endglobals'
+            optional('endglobals')
         ),
 
+        var_stmt: $ =>
+            seq(
+                optional('constant'),
+                field('type', $.id),
+                optional('array'),
+                $.var_decl,
+                repeat(seq(',', $.var_decl))
+            ),
+
+        var_decl: $ =>
+            seq(
+                field('name', $.id),
+                optional(seq('=', field('value', $.expr)))
+            ),
+
         expr: $ => choice(
-            $.id,
+            // Literals
             $.number,
             $.float,
             $.string,
+            $.id,
             $.function_call,
 
-            // https://learn.microsoft.com/en-us/cpp/cpp/cpp-built-in-operators-precedence-and-associativity?view=msvc-170
-            // Group 1 precedence, no associativity
-            // Scope resolution	::
+            // Array subscript
+            prec.left(PREC.SUBSCRIPT, seq($.expr, '[', $.expr, ']')),
 
-            // Group 2 precedence, left to right associativity
-            prec.left(2, seq($.expr, '[', $.expr, ']')), // Array subscript	[]
-            prec.left(2, seq('(', repeat($.expr), ')')), // Function call	()
-            prec.left(2, seq($.expr, '++')), // Postfix increment	++
-            prec.left(2, seq($.expr, '++')), // Postfix decrement	--
-            // Type name	typeid
-            // Constant type conversion	const_cast
-            // Dynamic type conversion	dynamic_cast
-            // Reinterpreted type conversion	reinterpret_cast
-            // Static type conversion	static_cast
+            // Postfix operators
+            prec.left(PREC.POSTFIX, seq($.expr, '++')),
+            prec.left(PREC.POSTFIX, seq($.expr, '--')),
 
-            // Group 3 precedence, right to left associativity
-            // Size of object or type	sizeof
-            prec.right(3, seq('++', $.expr)), // Prefix increment	++
-            prec.right(3, seq('--', $.expr)),  // Prefix decrement	--
-            // One's complement	~	compl
-            prec.right(3, seq('not', $.expr)), // Logical not	!	not
-            prec.right(3, seq('-', $.expr)),  // Unary negation	-
-            prec.right(3, seq('+', $.expr)), // Unary plus	+
-            // Address-of	&
-            // Indirection	*
-            // Create object	new
-            // Destroy object	delete
-            // Cast	()
+            // Prefix operators
+            prec.right(PREC.UNARY, seq('++', $.expr)),
+            prec.right(PREC.UNARY, seq('--', $.expr)),
+            prec.right(PREC.UNARY, seq('not', $.expr)),
+            prec.right(PREC.UNARY, seq('-', $.expr)),
+            prec.right(PREC.UNARY, seq('+', $.expr)),
 
-            // Group 4 precedence, left to right associativity
-            // Pointer-to-member (objects or pointers)	.* or ->*
+            // Multiplicative operators
+            prec.left(PREC.MULTIPLICATIVE, seq($.expr, '*', $.expr)),
+            prec.left(PREC.MULTIPLICATIVE, seq($.expr, '/', $.expr)),
 
-            // Group 5 precedence, left to right associativity (HIGHER precedence in tree-sitter)
-            prec.left(6, seq($.expr, '*', $.expr)), // Multiplication	*
-            prec.left(6, seq($.expr, '/', $.expr)), // Division	/
-            // Modulus	%
+            // Additive operators
+            prec.left(PREC.ADDITIVE, seq($.expr, '+', $.expr)),
+            prec.left(PREC.ADDITIVE, seq($.expr, '-', $.expr)),
 
-            // Group 6 precedence, left to right associativity (LOWER precedence in tree-sitter)
-            prec.left(5, seq($.expr, '+', $.expr)), // Addition	+
-            prec.left(5, seq($.expr, '-', $.expr)), // Subtraction	-
+            // Relational operators
+            prec.left(PREC.RELATIONAL, seq($.expr, '<', $.expr)),
+            prec.left(PREC.RELATIONAL, seq($.expr, '>', $.expr)),
+            prec.left(PREC.RELATIONAL, seq($.expr, '<=', $.expr)),
+            prec.left(PREC.RELATIONAL, seq($.expr, '>=', $.expr)),
 
-            // Group 7 precedence, left to right associativity
-            // Left shift	<<
-            // Right shift	>>
+            // Equality operators
+            prec.left(PREC.EQUALITY, seq($.expr, '==', $.expr)),
+            prec.left(PREC.EQUALITY, seq($.expr, '!=', $.expr)),
 
-            // Group 8 precedence, left to right associativity
-            prec.left(8, seq($.expr, '<', $.expr)), // Less than	<
-            prec.left(8, seq($.expr, '>', $.expr)), // Greater than	>
-            prec.left(8, seq($.expr, '<=', $.expr)), // Less than or equal to	<=
-            prec.left(8, seq($.expr, '>=', $.expr)), // Greater than or equal to	>=
+            // Logical operators
+            prec.left(PREC.LOGICAL_AND, seq($.expr, 'and', $.expr)),
+            prec.left(PREC.LOGICAL_OR, seq($.expr, 'or', $.expr)),
 
-            // Group 9 precedence, left to right associativity
-            prec.left(9, seq($.expr, '==', $.expr)), // Equality	==
-            prec.left(9, seq($.expr, '!=', $.expr)), // Inequality	!=	not_eq
-
-            // Group 10 precedence left to right associativity
-            // Bitwise AND	&	bitand
-
-            // Group 11 precedence, left to right associativity
-            // Bitwise exclusive OR	^	xor
-
-            // Group 12 precedence, left to right associativity
-            // Bitwise inclusive OR	|	bitor
-
-            // Group 13 precedence, left to right associativity
-            prec.left(14 /* in JASS */, seq($.expr, 'and', $.expr)), // Logical AND	&&	and
-
-            // Group 14 precedence, left to right associativity
-            prec.left(13 /* in JASS */, seq($.expr, 'or', $.expr)), // Logical OR	||	or
-
-            // Group 15 precedence, right to left associativity
-            // Conditional	? :
-            prec.right(15, seq($.expr, '=', $.expr)), // Assignment	=
-            // Multiplication assignment	*=
-            // Division assignment	/=
-            // Modulus assignment	%=
-            // Addition assignment	+=
-            // Subtraction assignment	-=
-            // Left-shift assignment	<<=
-            // Right-shift assignment	>>=
-            // Bitwise AND assignment	&=	and_eq
-            // Bitwise inclusive OR assignment	|=	or_eq
-            // Bitwise exclusive OR assignment	^=	xor_eq
-            // throw expression	throw
-
-            // Group 16 precedence, left to right associativity
-            prec.left(16, seq($.expr, ',', $.expr)), // Comma
-
+            // Assignment operator (lowest precedence)
+            prec.right(PREC.ASSIGNMENT, seq($.expr, '=', $.expr))
         ),
 
         number: _ => {
@@ -206,49 +156,17 @@ module.exports = grammar({
             ))
         },
 
-        var_stmt: $ =>
-            seq(
-                repeat(choice('local', 'constant')),
-                field('type', $.id),
-                optional('array'),
-                $.var_decl,
-                repeat(seq(',', $.var_decl))
-            ),
-
-        var_decl: $ => seq(
-            field('name', $.id),
-            optional(seq('=', $.expr))
-        ),
-
-        _var: $ =>
-            choice(
-                $.id,
-                seq($.id, '[', $.expr, ']')
-            ),
-
-        string: $ =>
-            seq(
-                field('start', alias($._string_start, 'string_start')),
-                field('content', optional(alias($._string_content, 'string_content'))),
-                field('end', alias($._string_end, 'string_end'))
-            ),
-
-        _statement: $ =>
-            choice(
-                $.var_stmt,
-                $.set_statement,
-                $.call_statement,
-                $.if_statement,
-                $.loop_statement,
-                $.return_statement,
-                $.exitwhen_statement,
-            ),
-
-        unary_operation: $ =>
-            prec.left(PREC.UNARY, seq(choice('not', '#', '-', '~'), $.expr)),
+        string: _ => token(seq(
+            '"',
+            repeat(choice(
+                /[^"\\]/,
+                seq('\\', /./)
+            )),
+            '"'
+        )),
 
         _id_list: $ =>
-            prec.right(PREC.COMMA, seq($.id, repeat(seq(/,\s*/, $.id)))),
+            seq($.id, repeat(seq(',', $.id))),
 
         return_statement: $ =>
             prec.right(seq('return', optional($.expr))),
@@ -256,42 +174,32 @@ module.exports = grammar({
         exitwhen_statement: $ =>
             seq('exitwhen', $.expr),
 
-        set_statement: $ =>
+        local_statement: $ =>
             seq(
-                'set',
-                field('variable', $.id),
-                optional(seq('[', field('index', $.expr), ']')),
-                '=',
-                field('value', $.expr)
-            ),
-
-        call_statement: $ =>
-            seq('call', $.function_call),
-
-        // Blocks {{{
-        loop_statement: $ =>
-            seq(
-                'loop',
-                repeat($._statement),
-                'endloop'
+                'local',
+                optional('constant'),
+                field('type', $.id),
+                optional('array'),
+                field('name', $.id),
+                optional(seq('=', field('value', $.expr)))
             ),
 
         if_statement: $ =>
             seq(
                 'if',
-                field('condition', $.expr),
+                optional(field('condition', $.expr)),
                 'then',
                 repeat($._statement),
                 repeat(
                     seq(
                         'elseif',
-                        field('condition', $.expr),
+                        optional(field('condition', $.expr)),
                         'then',
                         repeat($._statement)
                     )
                 ),
                 optional(seq('else', repeat($._statement))),
-                'endif'
+                optional('endif')
             ),
 
         function_start: () => 'function',
@@ -325,7 +233,7 @@ module.exports = grammar({
                 'returns',
                 field('return_type', choice('nothing', $.id)),
                 repeat($._statement),
-                'endfunction'
+                optional('endfunction')
             ),
 
         parameter_list: $ =>
@@ -357,23 +265,11 @@ module.exports = grammar({
         // }}}
 
 
-        // Comments {{{
-        // comment: ($) => choice(seq("--", /[^-].*\r?\n/), $._multi_comment),
-        comment: $ =>
-            choice(
-                seq(
-                    field('start', alias('//', 'comment_start')),
-                    field('content', alias(/[^\r\n]*/, 'comment_content'))
-                ),
-                seq(
-                    field('start', alias($._block_comment_start, 'comment_start')),
-                    field(
-                        'content',
-                        optional(alias($._block_comment_content, 'comment_content'))
-                    ),
-                    field('end', alias($._block_comment_end, 'comment_end'))
-                )
-            ),
+        // Comments
+        comment: _ => token(choice(
+            seq('//', /[^\r\n]*/),
+            seq('/*', /[^*]*\*+(?:[^/*][^*]*\*+)*/, '/')
+        )),
         // }}}
     },
 })
